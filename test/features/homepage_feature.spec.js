4f211f74849f2912bb18493bbd9b08513c7cac5b
Jonathan <j_passe@live.fr>
2016-09-16 16:53:50 +0100

    display sound spiking

--- test/features/homepage_feature.spec.js
+++ test/features/homepage_feature.spec.js
@@ -2,11 +2,23 @@ var Browser = require('zombie');
 
 Browser.localhost('example.com', 8000);
 
-describe('User visits signup page', function() {
+describe('User visits homepage', function() {
   var browser = new Browser();
 
+  before(function(done) {
+    browser.visit('/', done);
+  });
+
   it('should see welcome page', function() {
-    return browser.visit('/');
     browser.assert.text('title', 'Smart Tabs')
   });
+
+  it('should contain music canvas', function() {
+    browser.assert.element('#music-canvas')
+  });
+
+  it('should contain record and stop buttons', function() {
+    browser.assert.element('#recording-controllers')
+  });
+  
 });
